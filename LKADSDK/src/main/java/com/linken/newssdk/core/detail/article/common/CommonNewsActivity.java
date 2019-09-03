3a73608b299d56fc4da8a3d201b5e386ee541d0f
liuxihui <liuxihui168168@163.com>
2019-09-03 15:49:50 +0800

    头条

--- LKADSDK/src/main/java/com/linken/newssdk/core/detail/article/common/CommonNewsActivity.java
+++ LKADSDK/src/main/java/com/linken/newssdk/core/detail/article/common/CommonNewsActivity.java
@@ -1,5 +1,6 @@
 package com.linken.newssdk.core.detail.article.common;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.net.Uri;
@@ -7,14 +8,26 @@ import android.os.Build;
 import android.text.TextUtils;
 import android.util.Base64;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bytedance.sdk.openadsdk.AdSlot;
+import com.bytedance.sdk.openadsdk.TTAdConstant;
+import com.bytedance.sdk.openadsdk.TTAdDislike;
+import com.bytedance.sdk.openadsdk.TTAdNative;
+import com.bytedance.sdk.openadsdk.TTAppDownloadListener;
+import com.bytedance.sdk.openadsdk.TTImage;
+import com.bytedance.sdk.openadsdk.TTNativeAd;
 import com.linken.ad.data.AdvertisementCard;
 import com.linken.ad.data.RewardCard;
 import com.linken.newssdk.IntentConstants;
+import com.linken.newssdk.NewsConfig;
 import com.linken.newssdk.NewsFeedsSDK;
 import com.linken.newssdk.R;
 import com.linken.newssdk.SDKContants;
@@ -27,18 +40,23 @@ import com.linken.newssdk.data.ad.db.AdvertisementDbUtil;
 import com.linken.newssdk.data.card.base.Card;
 import com.linken.newssdk.data.news.INewsType;
 import com.linken.newssdk.export.INewsInfoCallback;
+import com.linken.newssdk.toutiao.TTAdManagerHolder;
 import com.linken.newssdk.utils.ContextUtils;
 import com.linken.newssdk.utils.DensityUtil;
 import com.linken.newssdk.utils.EncryptUtil;
 import com.linken.newssdk.utils.SPUtils;
 import com.linken.newssdk.utils.TimeUtil;
+import com.linken.newssdk.utils.ToastUtils;
+import com.linken.newssdk.utils.support.ImageLoaderHelper;
 import com.linken.newssdk.widget.views.CustomCountLayout;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static com.linken.newssdk.data.news.INewsType.NORMAL_NEWS_URL;
@@ -69,16 +87,28 @@ public abstract class CommonNewsActivity<P extends CommonNewsPresenter> extends 
     private boolean isFristPageFinish;//网页第一次加载完成
     private String docid;
     private String title;
+	
+	private FrameLayout mBannerContainer;
+    private TTAdNative mTTAdNative;
+    private Button mCreativeButton;
 
     @Override
     protected void initView() {
         mWebView = findViewById(R.id.web_container);
+        mBannerContainer = findViewById(R.id.web_banner);
+
+        //step2:创建TTAdNative对象
+        mTTAdNative = TTAdManagerHolder.get().createAdNative(this);
+        //step3:可选，强烈建议在合适的时机调用):申请部分权限，如read_phone_state,防止获取不了imei时候，下载类广告没有填充的问题。
+        TTAdManagerHolder.get().requestPermissionIfNecessary(this);
     }
 
     @Override
     protected void initData() {
         initWebView(this, mWebView);
         fetchData();
+
+        loadBannerAd(NewsConfig.CODE_ID_BANNER);
     }
 
     @Override
@@ -93,27 +123,6 @@ public abstract class CommonNewsActivity<P extends CommonNewsPresenter> extends 
         }
         handleStyleAndUrl();
         mUri = generateUri();
-
-        if (mCard != null) {
-            SDKContants.channel = mCard.channel;
-            if (Card.CTYPE_VIDEO_LIVE_CARD.equals(mCard.cType) || Card.CTYPE_VIDEO_CARD.equals(mCard.cType)) {
-                mType = INewsInfoCallback.TYPE_VIDEO;
-            } else if (Card.CTYPE_ADVERTISEMENT.equals(mCard.cType)) {
-                mType = INewsInfoCallback.TYPE_AD;
-            } else {
-                mType = INewsInfoCallback.TYPE_ARTICLE;
-            }
-        }
-        if (!TextUtils.isEmpty(mUri)) {
-            Uri uri = Uri.parse(mUri);
-            docid = uri.getQueryParameter("docid");
-            if (STYLE_HBRID_VIDEO == articleType) {
-                mType = INewsInfoCallback.TYPE_VIDEO;
-            } else {
-                mType = INewsInfoCallback.TYPE_ARTICLE;
-            }
-        }
-
     }
 
     private void handleStyleAndUrl() {
@@ -322,6 +331,9 @@ public abstract class CommonNewsActivity<P extends CommonNewsPresenter> extends 
             duration = System.currentTimeMillis() - landingPageStartTime + duration;
         }
         newsInfoCallback(INewsInfoCallback.TYPE_EVENT_DURATION, rewardNum, countDown, (int) (duration / 1000));
+		 if (mCreativeButton != null) {
+            mCreativeButton = null;
+        }
         super.onDestroy();
     }
 
@@ -534,4 +546,195 @@ public abstract class CommonNewsActivity<P extends CommonNewsPresenter> extends 
         SPUtils.put(INewsInfoCallback.REWARD_KEY, rewardNum);
         SPUtils.put(INewsInfoCallback.REWARD_TIME_KEY, rewardTime);
     }
+ @SuppressWarnings({"ALL", "SameParameterValue"})
+    private void loadBannerAd(String codeId) {
+        //step4:创建广告请求参数AdSlot,注意其中的setNativeAdtype方法，具体参数含义参考文档
+        final AdSlot adSlot = new AdSlot.Builder()
+                .setCodeId(codeId)
+                .setSupportDeepLink(true)
+                .setImageAcceptedSize(600, 257)
+                .setNativeAdType(AdSlot.TYPE_BANNER) //请求原生广告时候，请务必调用该方法，设置参数为TYPE_BANNER或TYPE_INTERACTION_AD
+                .setAdCount(1)
+                .build();
+
+        //step5:请求广告，对请求回调的广告作渲染处理
+        mTTAdNative.loadNativeAd(adSlot, new TTAdNative.NativeAdListener() {
+            @Override
+            public void onError(int code, String message) {
+//                TToast.show(NativeBannerActivity.this, "load error : " + code + ", " + message);
+            }
+
+            @Override
+            public void onNativeAdLoad(List<TTNativeAd> ads) {
+                if (ads.get(0) == null) {
+                    return;
+                }
+                View bannerView = LayoutInflater.from(CommonNewsActivity.this).inflate(R.layout.native_ad, mBannerContainer, false);
+                if (bannerView == null) {
+                    return;
+                }
+                if (mCreativeButton != null) {
+                    //防止内存泄漏
+                    mCreativeButton = null;
+                }
+                mBannerContainer.removeAllViews();
+                mBannerContainer.addView(bannerView);
+                //绑定原生广告的数据
+                setAdData(bannerView, ads.get(0));
+            }
+        });
+    }
+
+
+    @SuppressWarnings("RedundantCast")
+    private void setAdData(View nativeView, TTNativeAd nativeAd) {
+        ((TextView) nativeView.findViewById(R.id.tv_native_ad_title)).setText(nativeAd.getTitle());
+        ((TextView) nativeView.findViewById(R.id.tv_native_ad_desc)).setText(nativeAd.getDescription());
+        ImageView imgDislike = nativeView.findViewById(R.id.img_native_dislike);
+        bindDislikeAction(nativeAd, imgDislike);
+        if (nativeAd.getImageList() != null && !nativeAd.getImageList().isEmpty()) {
+            TTImage image = nativeAd.getImageList().get(0);
+            if (image != null && image.isValid()) {
+                ImageView im = nativeView.findViewById(R.id.iv_native_image);
+                ImageLoaderHelper.displayImage(im, image.getImageUrl());
+            }
+        }
+        TTImage icon = nativeAd.getIcon();
+        if (icon != null && icon.isValid()) {
+            ImageView im = nativeView.findViewById(R.id.iv_native_icon);
+            ImageLoaderHelper.displayImage(im, icon.getImageUrl());
+        }
+        mCreativeButton = (Button) nativeView.findViewById(R.id.btn_native_creative);
+        //可根据广告类型，为交互区域设置不同提示信息
+        switch (nativeAd.getInteractionType()) {
+            case TTAdConstant.INTERACTION_TYPE_DOWNLOAD:
+                //如果初始化ttAdManager.createAdNative(getApplicationContext())没有传入activity 则需要在此传activity，否则影响使用Dislike逻辑
+                nativeAd.setActivityForDownloadApp(this);
+                mCreativeButton.setVisibility(View.VISIBLE);
+                nativeAd.setDownloadListener(mDownloadListener); // 注册下载监听器
+                break;
+            case TTAdConstant.INTERACTION_TYPE_DIAL:
+                mCreativeButton.setVisibility(View.VISIBLE);
+                mCreativeButton.setText("立即拨打");
+                break;
+            case TTAdConstant.INTERACTION_TYPE_LANDING_PAGE:
+            case TTAdConstant.INTERACTION_TYPE_BROWSER:
+                mCreativeButton.setVisibility(View.VISIBLE);
+                mCreativeButton.setText("查看详情");
+                break;
+            default:
+                mCreativeButton.setVisibility(View.GONE);
+                ToastUtils.showShort(getApplicationContext(), "交互类型异常");
+        }
+
+        //可以被点击的view, 也可以把nativeView放进来意味整个广告区域可被点击
+        List<View> clickViewList = new ArrayList<>();
+        clickViewList.add(nativeView);
+
+        //触发创意广告的view（点击下载或拨打电话）
+        List<View> creativeViewList = new ArrayList<>();
+        //如果需要点击图文区域也能进行下载或者拨打电话动作，请将图文区域的view传入
+        //creativeViewList.add(nativeView);
+        creativeViewList.add(mCreativeButton);
+
+        //重要! 这个涉及到广告计费，必须正确调用。convertView必须使用ViewGroup。
+        nativeAd.registerViewForInteraction((ViewGroup) nativeView, clickViewList, creativeViewList, imgDislike, new TTNativeAd.AdInteractionListener() {
+            @Override
+            public void onAdClicked(View view, TTNativeAd ad) {
+                if (ad != null) {
+                    ToastUtils.showShort(getApplicationContext(), "广告" + ad.getTitle() + "被点击");
+                }
+            }
+
+            @Override
+            public void onAdCreativeClick(View view, TTNativeAd ad) {
+                if (ad != null) {
+                    ToastUtils.showShort(getApplicationContext(), "广告" + ad.getTitle() + "被创意按钮被点击");
+                }
+            }
+
+            @Override
+            public void onAdShow(TTNativeAd ad) {
+                if (ad != null) {
+                    ToastUtils.showShort(getApplicationContext(), "广告" + ad.getTitle() + "展示");
+                }
+            }
+        });
+
+    }
+
+    //接入网盟的dislike 逻辑，有助于提示广告精准投放度
+    private void bindDislikeAction(TTNativeAd ad, View dislikeView) {
+        final TTAdDislike ttAdDislike = ad.getDislikeDialog(this);
+        if (ttAdDislike != null) {
+            ttAdDislike.setDislikeInteractionCallback(new TTAdDislike.DislikeInteractionCallback() {
+                @Override
+                public void onSelected(int position, String value) {
+                    mBannerContainer.removeAllViews();
+                }
+
+                @Override
+                public void onCancel() {
+
+                }
+            });
+        }
+        dislikeView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ttAdDislike != null)
+                    ttAdDislike.showDislikeDialog();
+            }
+        });
+    }
+
+    private final TTAppDownloadListener mDownloadListener = new TTAppDownloadListener() {
+        @Override
+        public void onIdle() {
+            if (mCreativeButton != null) {
+                mCreativeButton.setText("开始下载");
+            }
+        }
+
+        @SuppressLint("SetTextI18n")
+        @Override
+        public void onDownloadActive(long totalBytes, long currBytes, String fileName, String appName) {
+            if (mCreativeButton != null) {
+                if (totalBytes <= 0L) {
+                    mCreativeButton.setText("下载中 percent: 0");
+                } else {
+                    mCreativeButton.setText("下载中 percent: " + (currBytes * 100 / totalBytes));
+                }
+            }
+        }
+
+        @SuppressLint("SetTextI18n")
+        @Override
+        public void onDownloadPaused(long totalBytes, long currBytes, String fileName, String appName) {
+            if (mCreativeButton != null) {
+                mCreativeButton.setText("下载暂停 percent: " + (currBytes * 100 / totalBytes));
+            }
+        }
+
+        @Override
+        public void onDownloadFailed(long totalBytes, long currBytes, String fileName, String appName) {
+            if (mCreativeButton != null) {
+                mCreativeButton.setText("重新下载");
+            }
+        }
+
+        @Override
+        public void onInstalled(String fileName, String appName) {
+            if (mCreativeButton != null) {
+                mCreativeButton.setText("点击打开");
+            }
+        }
+
+        @Override
+        public void onDownloadFinished(long totalBytes, String fileName, String appName) {
+            if (mCreativeButton != null) {
+                mCreativeButton.setText("点击安装");
+            }
+        }
+    };
 }
